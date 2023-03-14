2e8a6783e601ca6affc9a2ca1f1116175645ee92
Jose Balado <jose.balado@wapice.com>
2023-03-14 10:34:53 +0200

    Add a logger and database creation.

--- API/Program.cs
+++ API/Program.cs
@@ -28,4 +28,20 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    context.Database.Migrate();
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An eroor occurred during migration");
+
+    throw;
+}
+
 app.Run();
