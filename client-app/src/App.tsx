fa4b7f117ecaf4eb2e5a917d35266b9c6ae01f2e
Jose Balado <JoseBalado@users.noreply.github.com>
2023-03-20 17:56:01 +0200

    Add Cors to the Server.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [activities, setActivities] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/acitivites')
+    axios.get('http://localhost:5000/api/activities')
       .then(response => {
         console.log(response);
         setActivities(response.data);
